6ac3d84b597b8da17f4c82ad5324560043966558
George Varghese <george.varghese8815@gmail.com>
2016-06-27 19:37:19 +0530

    Add files via upload

---
+++ SQL-Advanced/Introduction to Indexing-191.py
@@ -0,0 +1,42 @@
+## 3. Explain query plan ##
+
+cursor = conn.cursor()
+query_plan_one = cursor.execute("EXPLAIN QUERY PLAN select * from facts where area > 40000").fetchall()
+query_plan_two = cursor.execute("EXPLAIN QUERY PLAN select area from facts where area > 40000").fetchall()
+query_plan_three = cursor.execute('''EXPLAIN QUERY PLAN select * from facts where name = "Czech Republic"''').fetchall()
+print(query_plan_one)
+print(query_plan_two)
+print(query_plan_three)
+
+## 5. Time complexity ##
+
+c = conn.cursor()
+query_plan_four = c.execute("Explain Query Plan Select * from facts where id = 20").fetchall()
+print(query_plan_four)
+
+## 7. Indexing ##
+
+india_index = conn.execute("select id from name_idx where name = 'India';").fetchall()[0][0]
+print(india_index)
+
+first_query_plan = conn.execute("explain query plan select id from name_idx where name = 'India';").fetchall()
+print(first_query_plan)
+
+india_row = conn.execute("select * from facts where id = ?;", (india_index,)).fetchall()
+print(india_row)
+
+second_query_plan = conn.execute("explain query plan select * from facts where id = ?;", (india_index,)).fetchall()
+print(second_query_plan)
+
+## 9. Query plan and indexing ##
+
+query_plan_five = conn.execute("explain query plan SELECT population from facts WHERE name = 'India';").fetchall()
+print(query_plan_five)
+
+## 10. All together now ##
+
+query_plan_six = conn.execute("explain query plan select * from facts where population > 10000 ;").fetchall()
+print(query_plan_six)
+conn.execute("create index if not exists pop_idx on facts(population)")
+query_plan_seven = conn.execute("explain query plan select * from facts where population > 10000 ;").fetchall()
+print(query_plan_seven)
---
+++ SQL-Advanced/Multi-column indexing-192.py
@@ -0,0 +1,31 @@
+## 1. Introduction ##
+
+c = conn.cursor()
+query_plan_one  = c.execute("EXPLAIN QUERY PLAN Select * from facts where population > 1000000 and population_growth  < 0.5").fetchall()
+print(query_plan_one)
+
+## 2. Query plan for multi-column queries ##
+
+import sqlite3
+conn = sqlite3.connect("factbook.db")
+conn.execute("create index if not exists pop_idx on facts(population);").fetchall()
+conn.execute("create index if not exists pop_growth_idx on facts(population_growth);").fetchall()
+
+query_plan_two = conn.execute("explain query plan select * from facts where population > 1000000 and population_growth < 0.05;").fetchall()
+print(query_plan_two)
+
+## 5. Creating a multi-column index ##
+
+conn.execute("create index if not exists pop_pop_growth_idx on facts(population, population_growth);")
+query_plan_three = conn.execute("explain query plan select * from facts where population > 1000000 and population_growth < 0.05;").fetchall()
+print(query_plan_three)
+
+## 6. Covering index ##
+
+query_plan_four = conn.execute("explain query plan select population, population_growth from facts where population > 1000000 and population_growth < 0.05;").fetchall()
+print(query_plan_four)
+
+## 7. Covering index for single column ##
+
+query_plan_five = conn.execute("explain query plan select population from facts where population > 1000000;").fetchall()
+print(query_plan_five)
